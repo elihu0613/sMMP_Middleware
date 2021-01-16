8b7167097080eb19f5e577fe46ebfa8f54dc7cdc
elihu <elihu0613@gmail.com>
2021-01-16 14:50:26 +0800

    initial

---
+++ 0.Middleware/EntryPoint.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Middleware.Pipes;
+
+namespace Middleware
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Middle.Server/EntryPoint.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Middle.Server
+{
+    class EntryPoint
+    {
+    }
+}
